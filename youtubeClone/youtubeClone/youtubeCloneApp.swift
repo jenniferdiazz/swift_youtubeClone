177118a64634419ba1b6ccddd66f393cb26fe19b
jennifer <jenniferdiaz@outlook.cl>
2023-08-07 17:58:46 -0400

    add first configuration

---
+++ youtubeClone/youtubeClone/youtubeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtubeCloneApp.swift
+//  youtubeClone
+//
+//  Created by Jennifer Diaz on 07-08-23.
+//
+
+import SwiftUI
+
+@main
+struct youtubeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
